5e1d09d7ef4f60e1e3cbe5a5be48b59c920bbd7a
zhangxiaohan <18010464520@qq.com>
2020-01-14 17:33:33 +0800

    图片上传优化

---
+++ ant-design-vue-jeecg/src/utils/fileUpload.js
@@ -0,0 +1,10 @@
+export function photoCheck(oFile,that){
+  var rFilter = /^(image\/bmp|image\/gif|image\/jpeg|image\/png|image\/tiff)$/i;
+  if (oFile && !rFilter.test(oFile.type)) {
+    that.$message.error('请上传文件非图片格式');
+    return false;
+  }
+  if (oFile && oFile.size > 5242880) {
+    that.$message.error('上传图片不能超过5M');
+    return false;
+  }}
