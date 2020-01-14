0565f98d9ee09e68fed98b63904706d7b918d732
zhangxiaohan <18010464520@qq.com>
2020-01-14 17:45:00 +0800

    组别管理

---
+++ ant-design-vue-jeecg/src/views/pd/modules/PdGroupModal.vue
@@ -0,0 +1,149 @@
+<template>
+  <a-modal
+    :title="title"
+    :width="width"
+    :visible="visible"
+    :confirmLoading="confirmLoading"
+    @ok="handleOk"
+    @cancel="handleCancel"
+    cancelText="关闭">
+    <a-spin :spinning="confirmLoading">
+      <a-form :form="form">
+
+        <a-form-item label="名称" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input @change="pinyinTran" v-decorator="[ 'name', validatorRules.name]" placeholder="请输入名称"></a-input>
+        </a-form-item>
+        <a-form-item label="拼音简码" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input v-decorator="[ 'py', validatorRules.py]" placeholder="请输入拼音简码"></a-input>
+        </a-form-item>
+        <a-form-item label="五笔简码" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input v-decorator="[ 'wb', validatorRules.wb]" placeholder="请输入五笔简码"></a-input>
+        </a-form-item>
+        <a-form-item label="自定义码" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input v-decorator="[ 'zdy', validatorRules.zdy]" placeholder="请输入自定义码"></a-input>
+        </a-form-item>
+        <a-form-item label="备注" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-input v-decorator="[ 'remarks', validatorRules.remarks]" placeholder="请输入备注"></a-input>
+        </a-form-item>
+
+      </a-form>
+    </a-spin>
+  </a-modal>
+</template>
+
+<script>
+
+  import { httpAction } from '@/api/manage'
+  import pick from 'lodash.pick'
+  import { validateDuplicateValue } from '@/utils/util'
+  import { makeWb } from '@/utils/wubi'
+
+  export default {
+    name: "PdGroupModal",
+    components: { 
+    },
+    data () {
+      return {
+        form: this.$form.createForm(this),
+        title:"操作",
+        width:800,
+        visible: false,
+        model: {},
+        labelCol: {
+          xs: { span: 24 },
+          sm: { span: 5 },
+        },
+        wrapperCol: {
+          xs: { span: 24 },
+          sm: { span: 16 },
+        },
+        confirmLoading: false,
+        validatorRules: {
+          name: {rules: [
+            {required: true, message: '请输入名称!'},
+          ]},
+          py: {rules: [
+          ]},
+          wb: {rules: [
+          ]},
+          zdy: {rules: [
+          ]},
+          remarks: {rules: [
+          ]},
+        },
+        url: {
+          add: "/pd/pdGroup/add",
+          edit: "/pd/pdGroup/edit",
+        }
+      }
+    },
+    created () {
+    },
+    methods: {
+      add () {
+        this.edit({});
+      },
+      edit (record) {
+        this.form.resetFields();
+        this.model = Object.assign({}, record);
+        this.visible = true;
+        this.$nextTick(() => {
+          this.form.setFieldsValue(pick(this.model,'name','py','wb','zdy','createTime','updateTime','remarks'))
+        })
+      },
+      close () {
+        this.$emit('close');
+        this.visible = false;
+      },
+      handleOk () {
+        const that = this;
+        // 触发表单验证
+        this.form.validateFields((err, values) => {
+          if (!err) {
+            that.confirmLoading = true;
+            let httpurl = '';
+            let method = '';
+            if(!this.model.id){
+              httpurl+=this.url.add;
+              method = 'post';
+            }else{
+              httpurl+=this.url.edit;
+               method = 'put';
+            }
+            let formData = Object.assign(this.model, values);
+            console.log("表单提交数据",formData)
+            httpAction(httpurl,formData,method).then((res)=>{
+              if(res.success){
+                that.$message.success(res.message);
+                that.$emit('ok');
+              }else{
+                that.$message.warning(res.message);
+              }
+            }).finally(() => {
+              that.confirmLoading = false;
+              that.close();
+            })
+          }
+         
+        })
+      },
+      handleCancel () {
+        this.close()
+      },
+      popupCallback(row){
+        this.form.setFieldsValue(pick(row,'name','py','wb','zdy','createTime','updateTime','remarks'))
+      },
+      pinyinTran(e){
+        var val = e.target.value;
+        let pinyin = require('js-pinyin');
+        pinyin.setOptions({checkPolyphone: false, charCase: 0});
+        //var py = pinyin.getFullChars(val);//获取全拼
+        var py = pinyin.getCamelChars(val);//获取简码
+        this.form.setFieldsValue({py:py});
+        var wb = makeWb(val);
+        this.form.setFieldsValue({wb:wb});//获取五笔简码
+      },
+      
+    }
+  }
+</script>
---
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/pd/mapper/PdGroupMapper.java
@@ -0,0 +1,17 @@
+package org.jeecg.modules.pd.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.jeecg.modules.pd.entity.PdGroup;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 产品组别
+ * @Author: zxh
+ * @Date:   2020-01-14
+ * @Version: V1.0
+ */
+public interface PdGroupMapper extends BaseMapper<PdGroup> {
+
+}
