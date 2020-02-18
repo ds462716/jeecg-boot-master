e2fc68ae3b07fd160518f2822527446a440a9941
muchuanbo <Muchuanbo123>
2020-02-18 10:24:40 +0800

    代码提交---mcb

--- ant-design-vue-jeecg/src/views/pd/PdProductStockQueryList.vue
+++ ant-design-vue-jeecg/src/views/pd/PdProductStockQueryList.vue
@@ -15,6 +15,11 @@
             </a-form-item>
           </a-col>
           <a-col :md="6" :sm="8">
+            <a-form-item label="产品编号">
+              <a-input placeholder="请输入产品编号" v-model="queryParam.productName"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :sm="8">
             <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
               <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
               <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
@@ -29,26 +34,12 @@
       </a-form>
     </div>
     <!-- 查询区域-END -->
-    <div class="numberWARAP">
-      <div class="total">总数量：<span id="pCount">{{this.validatorRules.pCount}}</span></div>
-      <div class="nearTime">近效期数量：<span id="jCount">{{this.validatorRules.jCount}}</span></div>
-      <div class="overTime">过期数量：<span id="gCount">{{this.validatorRules.gCount}}</span></div>
-    <!--  <div class="overTime">久存产品数量：<span id="isLcount">{{this.validatorRules.isLcount}}</span></div>
-      <div class="overTime">超出库房上下限产品数量：<span id="limtCount">{{this.validatorRules.limtCount}}</span></div>-->
-    </div>
     <!-- 操作按钮区域 -->
-     <div class="table-operator">
-      <a-button @click="handleUpdate('Up')" type="primary" icon="plus">批量设置库存上限</a-button>
-      <a-button @click="handleUpdate('Down')" type="primary" icon="plus">批量设置库存下限</a-button>
+    <div class="table-operator">
       <a-button type="primary" icon="download" @click="handleExportXls('库存明细')">导出</a-button>
     </div>
     <!-- table区域-begin -->
     <div>
-      <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
-        <i class="anticon anticon-info-circle ant-alert-icon"></i> 已选择 <a style="font-weight: 600">{{ selectedRowKeys.length }}</a>项
-        <a style="margin-left: 24px" @click="onClearSelected">清空</a>
-      </div>
-
       <a-table
         CLASS="changeColor"
         ref="table"
@@ -59,52 +50,26 @@
         :dataSource="dataSource"
         :pagination="ipagination"
         :loading="loading"
-        :rowClassName="setdataCss"
-        :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
         @change="handleTableChange">
-
-        <span slot="action" slot-scope="text, record">
-          <a @click="handleEdit(record)">库存明细</a>&nbsp;&nbsp;&nbsp;
-         <a  @click="handleRecordEdit(record)">出入库明细</a>
-        </span>
       </a-table>
     </div>
-<!--设置库存上下限弹出框-->
-    <pdProductStockTotal-modal ref="modalForm1" @ok="modalFormOk"></pdProductStockTotal-modal>
-    <!--库存明细查看页面-->
-    <pdProductStock-modal ref="stockForm" @ok="modalFormOk"></pdProductStock-modal>
-    <!--出入库明细查看页面-->
-    <pd-stock-record-detail-info-modal ref="stockForm2" @ok="modalFormOk"></pd-stock-record-detail-info-modal>
   </a-card>
 </template>
 <script>
 
   import { JeecgListMixin ,handleEdit} from '@/mixins/JeecgListMixin'
-  import PdProductStockTotalModal from './modules/PdProductStockTotalModal'
-  import PdProductStockModal from './modules/PdProductStockModal'
-  import PdStockRecordDetailInfoModal from './modules/PdStockRecordDetailInfoModal'
 
   import { getAction } from '@/api/manage'
   import {initDictOptions, filterMultiDictText} from '@/components/dict/JDictSelectUtil'
 
   export default {
-    name: "PdProductStockTotalList",
+    name: "PdProductStockQueryList",
     mixins:[JeecgListMixin],
     components: {
-      PdProductStockTotalModal,
-      PdProductStockModal,
-      PdStockRecordDetailInfoModal
     },
     data () {
       return {
-        description: '库存管理页面',
-        validatorRules: {
-          pCount:{},//总数量
-          gCount:{},//过期数量
-          jCount:{},//近效期数量
-          isLcount:{},//久存产品数量
-          limtCount:{},////超出库房上下限产品数量
-        },
+        description: '库存明细查询',
         // 表头
         columns: [
           {
@@ -133,6 +98,11 @@
             dataIndex: 'productNo'
           },
           {
+            title:'产品条码',
+            align:"center",
+            dataIndex: 'productNo'
+          },
+          {
             title:'规格',
             align:"center",
             dataIndex: 'spec'
@@ -143,54 +113,29 @@
             dataIndex: 'version'
           },
           {
-            title:'单位',
+            title:'批号',
             align:"center",
             dataIndex: 'unitName'
           },
           {
-            title:'库存上限',
+            title:'有效期',
             align:"center",
             dataIndex: 'limitUp'
           },
           {
-            title:'库存下限',
+            title:'数量',
             align:"center",
             dataIndex: 'limitDown'
           },
           {
-            title:'数量',
+            title:'生产厂家',
             align:"center",
             dataIndex: 'stockNum'
           },
           {
-            title:'是否过期',
+            title:'供应商',
             align:"center",
-            dataIndex: 'expire',
-            customRender:(text)=>{
-              if(!text){
-                return ''
-              }else{
-                return filterMultiDictText(this.dictOptions['expire'], text+"")
-              }
-            }
-          },
-          {
-            title:'是否永存',
-            align:"center",
-            dataIndex: 'isLong',
-            customRender:(text)=>{
-              if(!text){
-                return ''
-              }else{
-                return filterMultiDictText(this.dictOptions['isLong'], text+"")
-              }
-            }
-          },
-          {
-            title: '操作',
-            dataIndex: 'action',
-            align:"center",
-            scopedSlots: { customRender: 'action' },
+            dataIndex: 'stockNum'
           }
         ],
         url: {
@@ -272,11 +217,7 @@
             this.$set(this.dictOptions, 'isLong', res.result)
           }
         })
-      },
-      setdataCss(record,index) {
-        let expire = record.expire;
-        return "expire"+expire;
-      },
+      }
     }
   }
 </script>
@@ -287,15 +228,4 @@
   .changeColor .red td,.changeColor .red td a{color: red}
   /*.alert_close_btn:hover{color:#fff;border-bottom:1px solid #dedede}*/
   @import '~@assets/less/common.less'
-</style>
-
-<style>
-  .expire0{
-  }
-  .expire1{
-    background-color:#FFFFCC;
-  }
-  .expire2{
-    background-color:#FF3333;
-  }
 </style>
