5d6db8650af8acaae646f4d1bae3db274939f2bd
muchuanbo <Muchuanbo123>
2020-02-25 12:04:08 +0800

    代码提交---mcb

--- ant-design-vue-jeecg/src/views/pd/PdProductStockTotalList.vue
+++ ant-design-vue-jeecg/src/views/pd/PdProductStockTotalList.vue
@@ -60,6 +60,7 @@
         :pagination="ipagination"
         :loading="loading"
         :rowClassName="setdataCss"
+        :customRow="onClickRow"
         :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
         @change="handleTableChange">
 
@@ -277,6 +278,35 @@
         let expire = record.expStatus;
         return "expire"+expire;
       },
+      onClickRow(record) {
+        return {
+          on: {
+            click: (e) => {
+              //点击操作那一行不选中表格的checkbox
+              let pathArray = e.path;
+              //获取当前点击的是第几列
+              let td = pathArray[0];
+              let cellIndex = td.cellIndex;
+              //获取tr
+              let tr = pathArray[1];
+              //获取一共多少列
+              let lie = tr.childElementCount;
+              if(lie && cellIndex){
+                if(parseInt(lie)-parseInt(cellIndex)!=1){
+                  //操作那一行
+                  let recordId = record.id;
+                  let index = this.selectedRowKeys.indexOf(recordId);
+                  if(index>=0){
+                    this.selectedRowKeys.splice(index, 1);
+                  }else{
+                    this.selectedRowKeys.push(recordId);
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
     }
   }
 </script>
