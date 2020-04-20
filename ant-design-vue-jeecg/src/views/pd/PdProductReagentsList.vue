71f48077bc6fc3facf553e21171e7b398eaeeed4
zhangxiaohan <18010464520@qq.com>
2020-04-20 14:16:44 +0800

    产品加入试剂

--- ant-design-vue-jeecg/src/views/pd/PdProductReagentsList.vue
+++ ant-design-vue-jeecg/src/views/pd/PdProductReagentsList.vue
@@ -111,7 +111,7 @@
       <a-dropdown v-if="selectedRowKeys.length > 0">
         <a-menu slot="overlay">
           <a-menu-item key="1" @click="batchDel"><a-icon type="delete"/>删除</a-menu-item>
-          <a-menu-item key="2" @click="handleChargeCode"><a-icon type="edit"/>批量修改试剂收费代码</a-menu-item>
+          <a-menu-item key="2" @click="handleChargeCode"><a-icon type="edit"/>批量修改产品收费代码</a-menu-item>
         </a-menu>
         <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down" /></a-button>
       </a-dropdown>
