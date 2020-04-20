3be92a68933ed9e88e6d9ba1537a578577d71a4e
jxzlh <jxzlh1208@163.com>
2020-04-20 10:29:13 +0800

    入库增加业态

--- ant-design-vue-jeecg/src/views/pd/modules/PdStockRecordInModal.vue
+++ ant-design-vue-jeecg/src/views/pd/modules/PdStockRecordInModal.vue
@@ -28,7 +28,6 @@
               </a-col>
               <a-col :span="6">
                 <a-form-item label="入库类型" :labelCol="labelCol" :wrapperCol="wrapperCol">
-                  <!--<j-dict-select-tag-expand :disabled="disableSubmit" type="list" v-decorator="['inType', validatorRules.inType]" :trigger-change="true" dictCode="in_type" placeholder="请选择入库类型"/>-->
                   <j-dict-select-tag-expand disabled type="list" v-decorator="['inType', validatorRules.inType]" :trigger-change="true" dictCode="in_type" placeholder="请选择入库类型"/>
                 </a-form-item>
               </a-col>
@@ -67,6 +66,11 @@
                   >
                     <a-select-option v-for="d in supplierData" :key="d.value">{{d.text}}</a-select-option>
                   </a-select>
+                </a-form-item>
+              </a-col>
+              <a-col :span="6">
+                <a-form-item label="业态" :labelCol="labelCol" :wrapperCol="wrapperCol">
+                  <j-dict-select-tag-expand :disabled="disableSubmit" type="list" v-decorator="['format', validatorRules.format]" :trigger-change="true" dictCode="format" placeholder="请选择入库类型"/>
                 </a-form-item>
               </a-col>
             </a-row>
@@ -322,6 +326,7 @@
           recordNo:{},
           recordType:{},
           inType:{rules: [{required: true, message: '请选择入库类型!'}]},
+          format:{},
           // orderNo:{},
           mergeOrderNo:{},
           remarks:{},
@@ -485,7 +490,7 @@
 
             this.popModal.title="入库明细";
           let fieldval = pick(this.model,'recordNo','mergeOrderNo','inType','submitBy','submitByName','submitDate','remarks','inDepartId','supplierId',
-            'testResult','storageResult','temperature','humidity','remarks','refuseReason')
+            'testResult','storageResult','temperature','humidity','remarks','refuseReason','format')
           this.$nextTick(() => {
             this.form.setFieldsValue(fieldval);
           })
@@ -516,7 +521,7 @@
                 this.initData.humidity = "50";
                 this.submitDateStr = res.result.submitDateStr;
                 let fieldval = pick(this.initData,'recordNo','mergeOrderNo','inType','submitBy','submitByName','submitDate','remarks','inDepartId','supplierId',
-                  'testResult','storageResult','temperature','humidity','remarks','refuseReason');
+                  'testResult','storageResult','temperature','humidity','remarks','refuseReason','format');
                 this.form.setFieldsValue(fieldval);
                 //获取光标
                 this.$refs['productNumberInput'].focus();
@@ -728,7 +733,7 @@
       },
       popupCallback(row){
         this.form.setFieldsValue(pick(row,'recordNo','mergeOrderNo','inType','submitBy','submitByName','submitDate','remarks','inDepartId','supplierId',
-          'testResult','storageResult','temperature','humidity','remarks','refuseReason'))
+          'testResult','storageResult','temperature','humidity','remarks','refuseReason','format'))
       },
 
       //-----------------供应商查询start
