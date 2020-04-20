72dc12cd44f31e7ae2500f1c99ec83690c74d17f
zhangxiaohan <18010464520@qq.com>
2020-04-20 10:05:21 +0800

    产品加入试剂

--- jeecg-boot/db/spd.sql
+++ jeecg-boot/db/spd.sql
@@ -4,3 +4,6 @@ ADD COLUMN `product_flag`  varchar(1) NOT NULL COMMENT '0产品，1试剂' AFTER
 
 -- add by jiangxz 20200420 入库记录表增加业态字段
 ALTER TABLE `pd_stock_record` ADD COLUMN `format` varchar(32) NULL COMMENT '业态' AFTER `remarks`;
+
+-- add by zxh 20200420 产品加入试剂
+INSERT INTO `sys_permission` VALUES ('1252044092885708801', '1210107255254798338', '试剂管理', '/pd/PdProductReagents', 'pd/PdProductReagentsList', null, null, '1', '0', null, '1', '0.20', '0', null, '1', '1', '0', '0', null, 'admin', '2020-04-20 09:19:08', 'admin', '2020-04-20 09:19:42', '0', '0', '1', '0');
