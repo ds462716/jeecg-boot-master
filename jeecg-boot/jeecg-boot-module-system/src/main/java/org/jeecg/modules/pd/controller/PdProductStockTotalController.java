88f9e4f125d4a75f141abe4421cfda4a8e7d1521
jxzlh <jxzlh1208@163.com>
2020-04-29 13:42:30 +0800

    库存选择器优化

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/pd/controller/PdProductStockTotalController.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/pd/controller/PdProductStockTotalController.java
@@ -480,12 +480,10 @@ public class PdProductStockTotalController {
 		LoginUser sysUser = (LoginUser) SecurityUtils.getSubject().getPrincipal();
 		productStock.setDepartId(sysUser.getCurrentDepartId());
 		productStock.setDepartParentId(sysUser.getDepartParentId());
+		productStock.setNestatStatus(PdConstant.STOCK_NESTAT_STATUS_1);
 		page = pdProductStockService.selectList(page,productStock);
 		return Result.ok(page);
 	}
-
-
-
 
 	/**
 	 *库存规格数量清零
