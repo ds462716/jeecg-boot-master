a81b0eb6d0c27ed8ee0eee0409196022369ff3c8
muchuanbo <Muchuanbo123>
2020-05-13 21:03:30 +0800

    mcb

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/quartz/job/HisChargeSfTaskJob.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/quartz/job/HisChargeSfTaskJob.java
@@ -9,7 +9,6 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 /**
