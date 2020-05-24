c46d9286a3b61d6eddddcdd74f8d10fc6ad1a4c9
1093976920@qq.com <jxzlh1208@163.com>
2020-05-24 10:31:44 +0800

    bug修复以及优化

--- ant-design-vue-jeecg/src/views/pd/modules/PdRejectedModal.vue
+++ ant-design-vue-jeecg/src/views/pd/modules/PdRejectedModal.vue
@@ -435,7 +435,7 @@
           this.$message.error("请选择供应商！");
           return;
         }
-        this.$refs.pdChooseProductStockListModel.show({supplierId:supplierId});
+        this.$refs.pdChooseProductStockListModel.show({supplierId:supplierId,nestatStatus:"1"});
       },
       // 选择产品后返回
       returnProductStockData(data) {
