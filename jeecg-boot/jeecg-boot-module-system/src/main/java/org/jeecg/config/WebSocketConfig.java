8f3072860263257e310845fc68dc9367eb4adc34
zhangxiaohan <18010464520@qq.com>
2020-05-25 10:54:52 +0800

    代码优化

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/config/WebSocketConfig.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/config/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package org.jeecg.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 public class WebSocketConfig {
@@ -12,5 +14,5 @@ public class WebSocketConfig {
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
     }
-    
+
 }
