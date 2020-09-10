99ed1b4a9f76ff7c076105e612988bd4f3735cfc
zhangxiaohan <18010464520@qq.com>
2020-09-10 19:36:48 +0800

    赣州肿瘤医院上线

--- jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/external/ganzhouzhongliu/service/impl/ExHisChargeInfGzzlServiceImpl.java
+++ jeecg-boot/jeecg-boot-module-system/src/main/java/org/jeecg/modules/external/ganzhouzhongliu/service/impl/ExHisChargeInfGzzlServiceImpl.java
@@ -21,7 +21,7 @@ import java.util.List;
  * @Version: V1.0
  */
 @Service
-@DS("multi-datasource1")
+@DS("multi-datasourceGZZL")
 public class ExHisChargeInfGzzlServiceImpl extends ServiceImpl<ExHisChargeInfGzzlMapper, ExHisZyChargeInfGzzl> implements IExHisChargeInfGzzlService {
     @Autowired
     private ExHisChargeInfGzzlMapper exHisChargeInfGzzlMapper;
